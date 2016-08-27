47479bfb8726b7bf4bf86b50bc73c52e02bbe50a
Phill Conrad <pconrad@cs.ucsb.edu>
2016-08-27 14:37:20 -0700

    add page2

--- webapp.py
+++ webapp.py
@@ -7,49 +7,14 @@ app = Flask(__name__)
 def render_main():
     return render_template('home.html')
 
-@app.route('/ctof')
-def render_ctof():
-    return render_template('ctof.html')
+@app.route('/page1')
+def render_page1():
+    return render_template('page1.html')
 
-@app.route('/ftoc')
-def render_ftoc():
-    return render_template('ftoc.html')
+@app.route('/page2')
+def render_page2():
+    return render_template('page2.html')
 
-@app.route('/mtokm')
-def render_mtokm():
-    return render_template('mtokm.html')
-
-@app.route('/ftoc_result')
-def render_ftoc_result():
-    try:
-        ftemp_result = float(request.args['fTemp'])
-        ctemp_result = ftoc(ftemp_result)
-        return render_template('ftoc_result.html', fTemp=ftemp_result, cTemp=ctemp_result)
-    except ValueError:
-        return "Sorry: something went wrong."
-
-@app.route('/ctof_result')
-def render_ctof_result():
-    try:
-        ctemp_result = float(request.args['cTemp'])
-        ftemp_result = ctof(ctemp_result)
-        return render_template('ctof_result.html', cTemp=ctemp_result, fTemp=ftemp_result)
-    except ValueError:
-        return "Sorry: something went wrong."
-
-@app.route('/mtokm_result')
-def render_mtokm_result():
-    try:
-        # You'll need some code here, and maybe some extra parameters in render_template below...
-        return render_template('mtokm.html')
-    except ValueError:
-        return "Sorry: something went wrong."
-
-def ftoc(ftemp):
-   return (ftemp-32.0)*(5.0/9.0)
-
-def ctof(ftemp):
-   return -42.0 # replace with correct formula
 
 # You'll probably want a basic function here to convert miles to kilometers too...
 
